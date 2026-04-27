065a29738a547c78d4c256207ff83db9f21751d3
Moshiur Rahman <122657993+moshiurrahmandeap11@users.noreply.github.com>
2026-04-27 19:25:54 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,12 +10,17 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
-     
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
         port: '',
         pathname: '/**', 
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
